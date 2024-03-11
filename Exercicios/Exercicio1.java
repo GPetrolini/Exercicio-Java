533f07a67c5c8a5dfa02893ecd4f9093a0cc8eb4
GPetrolini <154474487+GPetrolini@users.noreply.github.com>
2024-03-11 12:43:24 -0300

    Segundo commit

---
+++ Exercicios/Exercicio1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package exerciociosjava;
+
+/**
+ *
+ * @author Gustavo
+ */
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Exercicios/exerciohelloworld.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exerciociosjava;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Gustavo
+ */
+public class exerciohelloworld {
+    public static void main (String[] args){
+        JOptionPane.showMessageDialog(null,"Olá  mundo!");
+    }
+    
+}
